48ca4e94d653a3c9e9d3bac5698eceec7b855d5c
Azwan20 <triyadi20112001@gmail.com>
2024-12-14 21:21:13 +0800

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,21 +9,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <title>Xtreme Free Next Js Dashboard</title>
-        <meta name="description" content="Xtreme Free Next Js Dashboard" />
+        <title>Kalkulator BMI | Hitung Indeks Massa Tubuh</title>
+        <meta name="description" content="Gunakan Kalkulator BMI untuk menghitung indeks massa tubuh Anda dan mengetahui kategori berat badan ideal Anda. Cek kesehatan Anda sekarang!" />
+        <meta name="keywords" content="Kalkulator BMI, Body Mass Index, Hitung Berat Badan, Berat Badan Ideal, Kesehatan, BMI Online" />
+        <meta name="author" content="Nama Anda" />
+        <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Kalkulator BMI | Hitung Berat Badan Ideal" />
+        <meta property="og:description" content="Cek kategori berat badan ideal Anda dengan Kalkulator BMI. Mudah, cepat, dan akurat." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.domain-anda.com" />
+        <meta property="og:image" content="/images/thumbnail.jpg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://www.domain-anda.com" />
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
       </Head>
       <body>
         {/* <AuthProvider> */}
-          {children}
-          <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-            crossOrigin="anonymous"
-            defer
-          ></script>
+        {children}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          crossOrigin="anonymous"
+          defer
+        ></script>
         {/* </AuthProvider> */}
       </body>
     </html>
